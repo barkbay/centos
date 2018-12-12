2fd0e6d8236572b16f582e9cd02eabc055b872c6
Michael Morello <michael.morello@gmail.com>
2018-12-12 09:33:35 +0100

    Add docker-ce-18.09.0

--- script/update.sh
+++ script/update.sh
@@ -3,19 +3,12 @@
 if [[ $UPDATE  =~ true || $UPDATE =~ 1 || $UPDATE =~ yes ]]; then
     echo "==> Applying updates"
     yum -y update
-    yum -y install unzip python-httplib2 socat rsync bash-completion git epel-release
+    yum -y install unzip python-httplib2 socat rsync bash-completion git epel-release yum-utils
 
 if [[ $DOCKER  =~ true || $DOCKER =~ 1 || $DOCKER =~ yes ]]; then
     echo "==> Installing Docker"
-cat <<EOF >> /etc/yum.repos.d/docker.repo
-[dockerrepo]
-name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/7
-enabled=1
-gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg
-EOF
-       yum -y install docker-engine-17.05.0.ce-1.el7.centos libselinux-python device-mapper-libs ebtables
+    yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo  
+    yum -y install docker-ce-18.09.0-3.el7.x86_64 libselinux-python device-mapper-libs ebtables
    fi
 
     # reboot
